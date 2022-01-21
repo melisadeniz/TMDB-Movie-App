bf4511332158af2b8561d7326a68f8a95322cd1b
melisadeniz <melisadeniz95@gmail.com>
2022-01-21 14:41:10 +0300

    info

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,18 +4,16 @@ function About() {
     return (
         <div className='container m-5 text-dark'>
             <div>
-                <p>This page is created for capstone project given at UP School Front-end Development Program by <strong>Melisa Deniz</strong>.</p>
+                <p>Capstone project by <strong>Melisa Deniz</strong>.</p>
                 
             </div>
             <div>
                 <h4>Technologies</h4>
                 <ul>
                     <li>React</li>
+                    <li>React-Router-DOM</li>
                     <li>JavaScript</li>
-                    <li>HTML</li>
-                    <li>CSS</li>   
                     <li>Bootstrap</li>
-                    <li>Public API</li>
                     <li>Redux</li>
                 </ul>
             </div>
